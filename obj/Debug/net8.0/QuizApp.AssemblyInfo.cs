8e439cb3c235511cae0950739e4d217b5af92c17
Olivereliasandersen <oland0229@oslomet.no>
2025-09-29 12:33:54 +0200

    Merge

--- obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0185ed1ceb31a3da216e03b5cc49a6cef21c364c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f1444a80dcf680751847ba4bc5186836ffd0b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

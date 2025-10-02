0a895c84416f954b20f46a81a65de749f30d54fc
Olivereliasandersen <oland0229@oslomet.no>
2025-10-02 13:39:55 +0200

    merge

--- obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/QuizApp.AssemblyInfo.cs
@@ -13,7 +13,11 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
+<<<<<<< Updated upstream
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+010f1bfc0581cd978d6a482cffa09d17928118e4")]
+=======
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b2102ac0309230c70c5a9139a2f721df2154a757")]
+>>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("QuizApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

97386b0568af42e0ed179b66cd06b226a3bb8e90
TobiasAndreFriis <143395113+TobiasAndreFriis@users.noreply.github.com>
2025-11-04 11:28:36 +0100

    Stylet matching i frontend

--- quizapp/src/matching/MatchingCardQuizPage.tsx
+++ quizapp/src/matching/MatchingCardQuizPage.tsx
@@ -6,7 +6,8 @@ const API_URL = "http://localhost:5041";
 
 function MatchingCardQuizPage() {
   const [matchingCards, setMatchingCards] = useState<MatchingCard[]>([]);
-  const [loadingMatchingCards, setLoadingMatchingCards] = useState<boolean>(false);
+  const [loadingMatchingCards, setLoadingMatchingCards] =
+    useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
 
   const fetchMatchingCards = async () => {
@@ -14,7 +15,9 @@ function MatchingCardQuizPage() {
     setError(null);
 
     try {
-      const response = await fetch(`${API_URL}/api/matchingapi/getQuestions/${2}`);
+      const response = await fetch(
+        `${API_URL}/api/matchingapi/getQuestions/${2}`,
+      );
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
@@ -46,39 +49,45 @@ function MatchingCardQuizPage() {
     return <div>Error: {error}</div>;
   }
 
-    return (
+  return (
     <div>
-        <h1>Matching Question</h1>
-        {matchingCards.length > 0 ? (
-        <div>
-            {matchingCards.map((card) => (
+      <h1>Matching Question</h1>
+      {matchingCards.length > 0 ? (
+        <div className="matching-card-wrapper">
+          {matchingCards.map((card) => (
             <div key={card.matchingCardId}>
-                {/* Spørsmålstekst */}
-                <h3>{card.questionText}</h3>
-            
-                <div>
-                    <table>
-                        <td>
-                            {card.keys.map((key, i) => (
-                                <tr key={i}>{key}</tr>
-                            ))}
-                        </td>
-                        <td>
-                            {card.values.map((value, i) => (
-                                <tr key={i}>{value}</tr>
-                            ))}
-                        </td>
-                    </table>
-                </div>
+              {/* Spørsmålstekst */}
+              <h3>{card.questionText}</h3>
+              <hr />
+
+              <div className="matching-table-wrapper">
+                <table className="matching-table">
+                  <td className="matching-table-keys">
+                    {card.keys.map((key, i) => (
+                      <tr className="matching-table-keys-tr" key={i}>
+                        {key}
+                      </tr>
+                    ))}
+                  </td>
+                  <td className="matching-table-values">
+                    {card.values.map((value, i) => (
+                      <tr className="matching-table-values-tr" key={i}>
+                        {value}
+                      </tr>
+                    ))}
+                  </td>
+                </table>
+              </div>
             </div>
-            ))}
+          ))}
         </div>
-        ) : (
+      ) : (
         <h3>No matching cards found.</h3>
-        )}
-        <button>Next question</button>
+      )}
+      <button>Next question</button>
     </div>
-    );
+  );
 }
 
 export default MatchingCardQuizPage;
+

c38e526911f27f688ea3ccd45fe38060ba6e0d1f
Olivereliasandersen <oland0229@oslomet.no>
2025-11-17 17:20:59 +0100

    Begynt på Frontend for Authentication

--- quizapp/src/App.tsx
+++ quizapp/src/App.tsx
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { AuthProvider } from "./auth/AuthContext";
+import ProtectedRoute from "./auth/ProtectedRoute";
 import FlashCardQuizPage from "./flashCard/FlashCardQuizPage";
 import Quizzes from "./flashCard/Quizzes";
 import MatchingCardQuizPage from "./matching/MatchingCardQuizPage";
@@ -12,7 +14,9 @@ import MultipleChoiceQuizPage from './multipleChoice/MultipleChoiceQuizPage'
 import ManageMultipleChoiceQuizPage from './multipleChoice/ManageMultipleChoiceQuizPage'
 import ManageTrueFalseQuizPage from './trueFalse/ManageTrueFalseQuizPage'
 import TrueFalseQuizPage from "./trueFalse/TrueFalseQuizPage";
-import NavMenu from "./shared/NavMenu";
+import LoginPage from "./auth/LoginPage";
+import RegisterPage from "./auth/RegisterPage";
+import NavMenu from "./shared/NavMenu"; 
 
 
 const App: React.FC = () => {
@@ -20,8 +24,10 @@ const App: React.FC = () => {
     <Router>
       <NavMenu />
       <br /> <br />
-      <Routes>
+      <Routes /*element={<ProtectedRoute />} Sjekker Auth. Må putte dette rundt riktige ting*/>
         <Route path="/" element={<QuizzesPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
         <Route path="/quizCreate" element={<QuizCreatePage />} />
         <Route path="/quizManage" element={<QuizManagePage />} />
         <Route path="/flashCardQuizzes" element={<Quizzes />} />
@@ -33,7 +39,6 @@ const App: React.FC = () => {
         <Route path="/sequenceQuiz" element={<SequenceCardQuizPage />} />
         <Route path="/rankingQuiz" element={<RankingCardQuizPage />} />
         <Route path="/quizTrueFalse" element={<TrueFalseQuizPage />} />
-
       </Routes>
     </Router>
   );
